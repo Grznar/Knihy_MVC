d96159ed6e6e924c2cb74ead5c8eb5da12814f34
Jakub Černý <AsKronak@seznam.cz>
2025-01-05 07:33:56 +0100

    model, database connection, dbcontext

---
+++ KnihyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KnihyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnihyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
