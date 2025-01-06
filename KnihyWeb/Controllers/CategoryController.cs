e6af89158c6746a9d2afc54f92580adc6e22fbc4
Jakub Černý <AsKronak@seznam.cz>
2025-01-06 06:31:41 +0100

    Update
    Zacatek razor pages, custom validace, asp validace, summary, tempdata - js validace knihovna, Edit, Delete category,

--- KnihyWeb/Controllers/CategoryController.cs
+++ KnihyWeb/Controllers/CategoryController.cs
@@ -19,8 +19,93 @@ namespace KnihyWeb.Controllers
         }
         public IActionResult Create()
         {
-        return View();
+            return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display order can not match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+
+            return View();
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? catergoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (catergoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catergoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited succesfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? catergoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (catergoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catergoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            Category? obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index", "Category");
+
+
+
+
+
+        }
+
 
     }
 }
--- KnihyWebRazor_Temp/Models/Category.cs
+++ KnihyWebRazor_Temp/Models/Category.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
-namespace KnihyWeb.Models
+namespace KnihyWebRazor_Temp.Models
 {
     public class Category
     {
@@ -13,8 +13,9 @@ namespace KnihyWeb.Models
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage ="Display order must be between 1 - 100")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1 - 100")]
         public int DisplayOrder { get; set; }
     }
-    
+
 }
+
