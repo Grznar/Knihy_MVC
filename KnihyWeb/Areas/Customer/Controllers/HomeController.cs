a0ba4ec19420ecb732208df296e92d9111ce6f5b
Jakub Černý <AsKronak@seznam.cz>
2025-01-14 04:44:49 +0100

    Home page pro Customers, Home a Details views

--- KnihyWeb/Areas/Customer/Controllers/HomeController.cs
+++ KnihyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Knihy.DataAccess.Repository.IRepository;
 using Knihy.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,23 @@ namespace KnihyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
